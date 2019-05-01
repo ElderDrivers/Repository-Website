6f853298f0987b98eb0bcb06e16fd42746e1a427
MlgmXyysd <1204074991@qq.com>
2019-05-01 20:37:44 +0800

    Add site load page

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$("#get-started").click(function(){
+	$("#scroll-tab-1").removeClass("is-active");
+	$("#tab-1").removeClass("is-active");
+	$("#scroll-tab-2").addClass("is-active");
+	$("#tab-2").addClass("is-active");
+});
+$("#i-am-sure").click(function(){
+	$("#scroll-tab-2").removeClass("is-active");
+	$("#tab-2").removeClass("is-active");
+	$("#scroll-tab-3").addClass("is-active");
+	$("#tab-3").addClass("is-active");
+});
+window.onload = function() {
+	setTimeout(function() {
+ 	$("#site-loader").addClass("load-completed");
+	},500);
+}
